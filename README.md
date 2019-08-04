e763b81dcd75e97a8179ae0203a11adcb26b3eb0
Bruno Gama <652969+brunogama@users.noreply.github.com>
2019-08-04 11:52:32 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Haxx0r SHA512 PopClip Extension
+
+A complicated way to print hashed sha512 text.
+
+## What it does?
+
+It will get the selected text and will append an UNIX epoch time hashed in SHA512 to it. After that it re-hash it again using the same algorithm.
+
+## Why
+
+When I can't find the "delete account" button, I feed fake data into my account to try to keep my identity secure.
+
+
+## Icon
+
+Lock.svg from Feather Icons
+
+## License
+
+BEERWARE
---
+++ hash-it.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#  "THE BEER-WARE LICENSE" (Revision 42):
+#  <@brunogama> wrote this file.  As long as you retain this notice you
+#  can do whatever you want with this stuff. If we meet some day, and you think
+#  this stuff is worth it, you can buy me a beer in return.   Bruno Gama
+
+__POP_OUTPUT= "$POPCLIP_TEXT" 
+for i in $(seq 1 5); do
+  __SALT=$(/bin/echo -n $(date +%s) | /usr/bin/openssl dgst -sha512 | awk '{print $1}')
+  __POP_OUTPUT=$(/bin/echo -n "${__POP_OUTPUT} ${__SALT}" | /usr/bin/openssl dgst -sha512)
+  # echo $__POP_OUTPUT
+done
+
+echo $__POP_OUTPUT | awk '{print $1}'
+unset __POP_OUTPUT __SALT
